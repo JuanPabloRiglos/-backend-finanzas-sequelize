ce728c74118e198bd96de6d59d98790f04df2864
OhMyBox <juan@ohmybox.ar>
2026-01-14 09:54:58 -0300

    ✨ Feat: Agregamos logica de Cors
     Se implementa lista blanca que se setea desde variables de entorno. Funcion que otorga paso a peticiones sin origen (postman) o a peticiones en la lista

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,26 @@
 import express, { Application } from 'express';
+import cors from 'cors';
 //importaciones internas
 import { errorHandler } from './middlewares/errorHandler.js';
 import routes from './routes/index.js';
 
+const allowwedOrigin = process.env.ALLOWED_ORIGINS?.split(',') || [];
+
 export const app: Application = express();
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      if (allowwedOrigin.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error('No permitido por Cors'));
+      }
+    },
+    credentials: true,
+  })
+);
 
 //middlewares
 app.use(express.json());
